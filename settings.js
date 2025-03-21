1c38c7d7e534a007e2b4f10edd5b6cd8651008dc
Papijahn1 <chidera.k777@gmail.com>
2025-03-21 19:39:06 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,7 +8,7 @@ global.ytname = "DRK_ST_Tech"
 global.socialm = "GitHub: DRK-S-TEN"
 global.location = "America/Port-au-Prince"
 
-global.ownernumber = '7014462600'  //creator number
+global.ownernumber = '2347014462600'  //creator number
 global.ownername = '𝙎-𝙏𝞢𝞜' //owner name
 global.botname = 'DRK_ST_BUG' //name of the bot
 
